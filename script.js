d8e7547461bf02f0db838f79174b2ea4b90cce6f
etheve-anthony <etheve.anthony97429@gmail.com>
2023-04-24 14:36:35 +0400

    modif zoom

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var map = L.map('map').setView([-21.19941657701768, 55.40958927791028], 20);
 
 L.tileLayer('https://tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey=7a0c2b23260c48929bee4682f83a4982', {
-    maxZoom: 19,
+    maxZoom: 22,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 

d4b34e43bf45f46aa1c25ce1f751194db3341b06
etheve-anthony <etheve.anthony97429@gmail.com>
2023-04-25 20:45:59 +0400

    aa

---
+++ script2.js
@@ -0,0 +1,61 @@
+// import Samsung, { APPS, KEYS } from 'samsung-tv-control'
+const { Samsung, KEYS, APPS } = require('samsung-tv-control')
+
+const config = {
+  debug: true, // Default: false
+  ip: '192.168.1.6',
+  mac: '9c:8c:6e:ab:b9:e6',
+  nameApp: 'NodeJS-Test', // Default: NodeJS
+  port: 8001, // Default: 8002
+  token: '',
+}
+
+const control = new Samsung(config)
+
+control.turnOn()
+control
+  .isAvailable()
+  .then(() => {
+    // Get token for API
+    control.getToken((token) => {
+      console.info('# Response getToken:', token)
+    })
+
+    // Send key to TV
+    control.sendKey(KEYS.KEY_HOME, function (err, res) {
+      if (err) {
+        throw new Error(err)
+      } else {
+        console.log(res)
+      }
+    })
+
+    // Get all installed apps from TV
+    control.getAppsFromTV((err, res) => {
+      if (!err) {
+        console.log('# Response getAppsFromTV', res)
+      }
+    })
+
+    // Get app icon by iconPath which you can get from getAppsFromTV
+    control.getAppIcon(
+      `/opt/share/webappservice/apps_icon/FirstScreen/${APPS.YouTube}/250x250.png`,
+      (err, res) => {
+        if (!err) {
+          console.log('# Response getAppIcon', res)
+        }
+      },
+    )
+
+    // Open app by appId which you can get from getAppsFromTV
+    control.openApp(APPS.YouTube, (err, res) => {
+      if (!err) {
+        console.log('# Response openApp', res)
+      }
+    })
+
+    // Control will keep connection for next messages in 1 minute
+    // If you would like to close it immediately, you can use `closeConnection()`
+    control.closeConnection()
+  })
+  .catch((e) => console.error(e))
